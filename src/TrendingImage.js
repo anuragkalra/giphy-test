c85f30a620608cb3ead9ada7d9ccb7ec987b8d28
anuragkalra <anuragkalra@outlook.com>
2018-12-30 21:26:00 -0500

    integrated onhover titles for trendingimage components

--- src/TrendingImage.js
+++ src/TrendingImage.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './TrendingImage.css';
 
 class TrendingImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false,
+    }
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
+  }
+
+  mouseOver() {
+    this.setState(
+      {hover: true}
+    );
+  }
+
+  mouseOut() {
+    this.setState(
+      {hover: false}
+    );
+  }
+
   render() {
     return (
-      <div>
+      <div onMouseOver={this.mouseOver} onMouseLeave={this.mouseOut}>
         <img src={this.props.item.images.preview_gif.url} alt={this.props.item.title}/>
-        <h1>{this.props.name}</h1>
+        {this.state.hover ? <h2 id="1">{this.props.item.title}</h2> : null}
       </div>
     )
   }
